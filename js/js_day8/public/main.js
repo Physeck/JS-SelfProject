2638cc062ecc47fa5d70a91dca29e969990a86e4
Physeck <justintjandx@gmail.com>
2026-03-06 19:56:48 +0700

    added canvas js logic and ui

--- js/js_day8/public/main.js
+++ js/js_day8/public/main.js
@@ -8,3 +8,46 @@ const typed = new Typed("#mainHeader", {
 
 
 // -----
+
+const canvas = document.getElementById("draw");
+const ctx = canvas.getContext("2d");
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
+const clearCanvas = document.getElementById("clearBtn");
+
+clearCanvas.addEventListener('click', () => {
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+});
+
+ctx.strokeStyle = '#000';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 8;
+// ctx.globalCompositeOperation = 'multiply';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+
+function draw(e) {
+    if (!isDrawing) return;
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+
+    hue++;
+    if(hue>360){
+        hue = 0;
+    }
+}
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
