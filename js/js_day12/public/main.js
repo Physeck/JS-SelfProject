0e486357c9628c6c0bbafbade877cfe779fe748b
Physeck <justintjandx@gmail.com>
2026-03-09 13:37:49 +0700

    added KONAMI Code JS

--- js/js_day12/public/main.js
+++ js/js_day12/public/main.js
@@ -8,4 +8,20 @@ const typed = new Typed("#mainHeader", {
 
 
 // -----
+const pressed = [];
+const secretCode = 'justin';
 
+window.addEventListener('keyup', (e) => {
+    console.log(e.key);
+    pressed.push(e.key);
+    // pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length); - conventional
+    if (pressed.length > secretCode.length) { // - modern
+        pressed.shift(); // remove the first (oldest) item
+    }
+
+    if (pressed.join('').includes(secretCode)) {
+        alert('FNC Physeck#BYEBY - VALORANT IGN');
+        cornify_add();
+    }
+    console.log(pressed);
+});
