03605baa61fefa3af6256ae36e8a61bc33581ca5
Physeck <justintjandx@gmail.com>
2026-03-11 14:03:59 +0700

    added js and images

--- js/js_day13/public/main.js
+++ js/js_day13/public/main.js
@@ -8,4 +8,33 @@ const typed = new Typed("#mainHeader", {
 
 
 // -----
+function debounce(func, timeout = 20) {
+    let timer;
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => { func.apply(this, args); }, timeout);
+    };
+}
+
+const sliderImg = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+    // console.log(window.scrollY);
+    sliderImg.forEach(sliderImg => {
+        //half of img width
+        const slideInAt = (window.scrollY + window.innerHeight) - sliderImg.height / 2;
+        //bottom of img width
+        const imgBottom = sliderImg.offsetTop + sliderImg.height;
+        const isHalfShown = slideInAt > sliderImg.offsetTop;
+        const isNotScrolledPast = window.scrollY < imgBottom;
+        if (isHalfShown && isNotScrolledPast) {
+            sliderImg.classList.add('active');
+        } else {
+            sliderImg.classList.remove('active');
+        }
+    });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
+
 
