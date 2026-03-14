1c10e945eb00a1605c96f35a8e46c298c92a0557
Physeck <justintjandx@gmail.com>
2026-03-14 14:42:31 +0700

    addded js for tally count

--- js/js_day18/public/main.js
+++ js/js_day18/public/main.js
@@ -8,4 +8,23 @@ const typed = new Typed("#mainHeader", {
 
 
 // -----
+const timeArr = [...document.querySelectorAll('[data-time]')];
+
+const seconds = timeArr
+    .map(node => node.dataset.time)
+    .map(timecode => {
+        const [min, sec] = timecode.split(':').map(parseFloat);
+        return (min * 60) + sec;
+    })
+    .reduce((total, vidSec) => total + vidSec);
+
+// console.log(seconds);
+
+const hLeft = Math.floor(seconds / 3600);
+let secLeft = seconds % 3600;
+
+const minLeft = Math.floor(secLeft / 60);
+secLeft = secLeft % 60;
+
+console.log(`${hLeft}h ${minLeft}m ${secLeft}s`);
 
