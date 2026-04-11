29d7636b103525010d8a44846a5738bd8295a2d1
Physeck <justintjandx@gmail.com>
2026-04-11 19:14:40 +0700

    day 24 commit

--- js/js_day24/public/main.js
+++ js/js_day24/public/main.js
@@ -8,4 +8,15 @@ const typed = new Typed("#mainHeader", {
 
 
 // -----
+const divs = document.querySelectorAll('div');
 
+function logText(e) {
+    console.log(this.classList.value);
+    // e.stopPropagation();
+
+}
+
+divs.forEach(div => div.addEventListener('click', logText, {
+    capture: false,
+    once: true //after click, event listener will automatically be removed (1x use only)
+}));
