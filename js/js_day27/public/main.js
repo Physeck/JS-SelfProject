cd03f7e3c72ef222bd24d1b1a5b57577d93de6e5
Physeck <justintjandx@gmail.com>
2026-04-14 20:21:57 +0700

    day 27 commit

--- js/js_day27/public/main.js
+++ js/js_day27/public/main.js
@@ -8,4 +8,22 @@ const typed = new Typed("#mainHeader", {
 
 
 // -----
+const speed = document.querySelector('.speed');
+const bar = document.querySelector('.speed-bar');
+const video = document.querySelector('.flex');
 
+speed.addEventListener('mousemove', function (e) {
+    const y = e.pageY - this.offsetTop;
+    const percentage = y / this.offsetHeight;
+    const min = 0.5;
+    const max = 5;
+    const height = Math.round(percentage * 100) + '%';
+    const playbackRate = percentage * (max - min) + min;
+    
+    bar.style.height = height;
+    bar.textContent = playbackRate.toFixed(1) + 'x';
+
+    video.playbackRate =  playbackRate;
+
+
+});
