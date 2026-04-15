af3e0ef7ac176c0d4bc7934a4629c5a3d1e91162
Physeck <justintjandx@gmail.com>
2026-04-15 19:26:57 +0700

    day 28 commit

--- js/js_day28/public/main.js
+++ js/js_day28/public/main.js
@@ -8,4 +8,56 @@ const typed = new Typed("#mainHeader", {
 
 
 // -----
+let countDown;
+const timerDisplay = document.querySelector('.timer');
+const timeThen = document.querySelector('.time-then');
+const buttons = document.querySelectorAll('[data-time]')
+
+function timer(second) {
+    clearInterval(countDown);
+    const now = Date.now();
+    const then = now + second * 1000;
+    displayTimeLeft(second);
+    displayTimeThen(then);
+    countDown = setInterval(() => {
+        const secLeft = Math.round((then - Date.now()) / 1000);
+        if (secLeft < 0) {
+            clearInterval(countDown);
+            return;
+        }
+        displayTimeLeft(secLeft);
+    }, 1000);
+}
+
+function displayTimeLeft(second) {
+    const hours = Math.floor(second / 3600);
+    const minutes = Math.floor((second % 3600) / 60);
+    const secLeft = second % 60;
+    const display = `${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}:${secLeft < 10 ? '0' : ''}${secLeft}`;
+    // document.title = display;
+    timerDisplay.textContent = display;
+}
+
+function displayTimeThen(second) {
+    const now = new Date(second);
+    console.log(now);
+    const hours = now.getHours();
+    //preference for adjusted time
+    // const adjustedTime = hours > 12 ? hours - 12 : hours;
+
+    const minutes = now.getMinutes();
+    timeThen.textContent = `${hours}:${minutes < 10 ? '0' : ''}${minutes}`
+}
+
+function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.minutes_form.addEventListener('submit', function (e) {
+    const min = (this.minutes.value) * 60;
+    e.preventDefault();
+    timer(min);
+});
 
